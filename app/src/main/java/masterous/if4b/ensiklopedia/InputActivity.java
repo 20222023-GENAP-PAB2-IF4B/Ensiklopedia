654780cf6c730fdf34f18a9323f79f12961a5a04
Yohannes Masterous <yohannes.masterous@gmail.com>
2023-03-30 11:42:39 +0700

    Update activity_input.xml

--- app/src/main/java/masterous/if4b/ensiklopedia/InputActivity.java
+++ app/src/main/java/masterous/if4b/ensiklopedia/InputActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import masterous.if4b.ensiklopedia.databinding.ActivityInputBinding;
+
 public class InputActivity extends AppCompatActivity {
+    private ActivityInputBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_input);
+
+        binding = ActivityInputBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
